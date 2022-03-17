462f18b8b88dc349cb0bc43a169db8b9554c93ff
KevinBR117 <kevinbarbozarodriguez117@gmail.com>
2022-03-17 10:36:41 -0600

    hover en botones y columna

--- javascript/eventos.js
+++ javascript/eventos.js
@@ -6,22 +6,11 @@ $(document).ready(function(){
     setTimeout(function(){
         elemento.fadeOut('swing');
         
-    },3000);
-   /*  elemento.remove(); */
-    
-    // columna inicio
-    
-    /* setTimeout(function() {
-        elemento.css("justify-content","start").css("align-items","flex-start");
-        elemento.removeClass("container-fluid p-5");
-        elemento.children("#imagen_logo").css("width","15%").css("height","auto").css("transform", "translateX(25%)")
-        elemento.children("#imagen_logo").appendTo("#logo_super_columna");
-        elemento.fadeIn("swing");
-    }, 5000); */
+    },1000);
 
     //desplegar contenido del boton conocenos
     var detalle_contenido = $("#detalle_contenido");
-    
+  
     var btn_conocenos = $("#btn_conocenos");
     btn_conocenos.hover(function () {
             detalle_contenido.css("z-index","1");
@@ -29,6 +18,29 @@ $(document).ready(function(){
             detalle_contenido.css("z-index","-1")
         }
     );
+    //desplegar contenido del boton prestamo
+    var detalle_contenido_1 = $("#detalle_contenido_1");
+  
+    var btn_prestamo = $("#btn_prestamo");
+    
+    btn_prestamo.hover(function () {
+            detalle_contenido_1.css("z-index","1 !important");
+        }, function () {
+            detalle_contenido_1.css("z-index","-1")
+        }
+    );
+
+
+    $("#btn_acercaDe").hover(function () { 
+        $("#detalle_contenido_2").css("z-index","1");   
+        }, function(){
+            (this).css("z-index","1");
+        }
+    );
+
+    
+
 
 });
+
 
