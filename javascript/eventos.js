baa327c11c0afd5fdfa0550dcc1c172eacb7d71d
KevinBR117 <kevinbarbozarodriguez117@gmail.com>
2022-03-17 14:24:31 -0600

    se añadio pestaña

--- javascript/eventos.js
+++ javascript/eventos.js
@@ -8,39 +8,6 @@ $(document).ready(function(){
         
     },1000);
 
-    //desplegar contenido del boton conocenos
-    var detalle_contenido = $("#detalle_contenido");
-  
-    var btn_conocenos = $("#btn_conocenos");
-    btn_conocenos.hover(function () {
-            detalle_contenido.css("z-index","1");
-        }, function () {
-            detalle_contenido.css("z-index","-1")
-        }
-    );
-    //desplegar contenido del boton prestamo
-    var detalle_contenido_1 = $("#detalle_contenido_1");
-  
-    var btn_prestamo = $("#btn_prestamo");
-    
-    btn_prestamo.hover(function () {
-            detalle_contenido_1.css("z-index","1 !important");
-        }, function () {
-            detalle_contenido_1.css("z-index","-1")
-        }
-    );
-
-
-    $("#btn_acercaDe").hover(function () { 
-        $("#detalle_contenido_2").css("z-index","1");   
-        }, function(){
-            (this).css("z-index","1");
-        }
-    );
-
-    
-
-
 });
 
 
