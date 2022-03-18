1244999a8f2e5e41ab1a47d5313673d95cd9f563
KevinBR117 <kevinbarbozarodriguez117@gmail.com>
2022-03-18 12:43:00 -0600

    pantalla de inicio completada

--- javascript/eventos.js
+++ javascript/eventos.js
@@ -1,13 +1,29 @@
-$(document).ready(function(){
-    console.log("estamos listos")
+$(document).ready(function () {
     
     //animacion logotipo
     var elemento = $("#logo_superpuesto");
-    setTimeout(function(){
+    setTimeout(function () {
         elemento.fadeOut('swing');
-        
-    },1000);
+
+    }, 1000);
+
 
 });
+var numero = 0;
+console.log(numero);
+
+var reloj = window.setInterval(function () {
+    document.getElementById("numeroVar").innerHTML = numero;
+    if (numero < 25) {
+        numero++;
+    }
+    else {
+        clearInterval(reloj);
+    }
+
+
+}, 1000);
+
+
 
 
