8b785dd5a42cec82897d320fbc1f33219881e9b0
KevinBR117 <kevinbarbozarodriguez117@gmail.com>
2022-03-22 15:37:23 -0600

    se añadio el sidebar

--- javascript/eventos.js
+++ javascript/eventos.js
@@ -1,29 +1,38 @@
 $(document).ready(function () {
-    
+
     //animacion logotipo
     var elemento = $("#logo_superpuesto");
     setTimeout(function () {
         elemento.fadeOut('swing');
 
     }, 1000);
-
-
 });
+
+/* animacion para sidebar */
+console.log("data")
+var btnToggle = document.getElementById("bar");
+console.log(btnToggle);
+
+btnToggle.addEventListener("click", function () {
+    document.getElementById("sidebar").classList.toggle("active")
+});
+
+/* animacion pagina conocenos */
 var numero = 0;
 console.log(numero);
+if (document.getElementById("numeroVar") != null) {
 
-var reloj = window.setInterval(function () {
-    document.getElementById("numeroVar").innerHTML = numero;
-    if (numero < 25) {
-        numero++;
-    }
-    else {
-        clearInterval(reloj);
-    }
+    var reloj = window.setInterval(function () {
+        document.getElementById("numeroVar").innerHTML = numero;
+        if (numero < 25) {
+            numero++;
+        }
+        else {
+            clearInterval(reloj);
+        }
 
-
-}, 1000);
-
+    }, 1000);
+}
 
 
 
