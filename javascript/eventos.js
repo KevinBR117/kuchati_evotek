46ceb2795454914ed74a22a2cfab7192a4860368
KevinBR117 <kevinbarbozarodriguez117@gmail.com>
2022-03-23 14:06:58 -0600

    actuaizacion side bar y paginas

--- javascript/eventos.js
+++ javascript/eventos.js
@@ -17,10 +17,45 @@ btnToggle.addEventListener("click", function () {
     document.getElementById("sidebar").classList.toggle("active")
 });
 
+/* botonoes sidebar */
+var itm_inicio = document.getElementById("li_inicio");
+var itm_conocenos = document.getElementById("li_conocenos");
+console.log(itm_conocenos);
+var itm_prestamo = document.getElementById("li_prestamo");
+var itm_acerca = document.getElementById("li_acerca");
+var itm_procesos = document.getElementById("li_procesos");
+var itm_arrendamiento = document.getElementById("li_arrendamiento");
+
+itm_inicio.addEventListener("click",function (){
+    window.location.href = "index.html"
+})
+
+itm_conocenos.addEventListener("click",function(){
+    console.log("clicked")
+    window.location.href = "conocenos.html"
+})
+
+itm_prestamo.addEventListener("click", function () {
+    window.location.href = "prestamo.html"
+})
+
+itm_acerca.addEventListener("click", function(){
+    window.location.href = "acercaDe.html"
+})
+
+itm_procesos.addEventListener("click", function () {  
+    window.location.href = "procesos.html"
+})
+
+itm_arrendamiento.addEventListener("click", function(){
+    window.location.href = "arrendamiento.html"
+})
+
+
 /* animacion pagina conocenos */
 var numero = 0;
 console.log(numero);
-if (document.getElementById("numeroVar") != null) {
+/* if (document.getElementById("numeroVar") != null) { */
 
     var reloj = window.setInterval(function () {
         document.getElementById("numeroVar").innerHTML = numero;
@@ -32,7 +67,7 @@ if (document.getElementById("numeroVar") != null) {
         }
 
     }, 1000);
-}
+/* } */
 
 
 
